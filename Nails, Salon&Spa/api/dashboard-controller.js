a8e70bf6488a5d9d9d477bf903960ff549542be4
RBmasujer <randyboymasujer@gmail.com>
2026-06-21 04:49:58 +0800

    Updated dashboard-controller.js

---
+++ Nails, Salon&Spa/api/dashboard-controller.js
@@ -0,0 +1,2 @@
+//this handles the connection of the chatroom between users
+//this js also handles cache data in memory for instant reads, refresh it on an interval/visibility change so it stays live if left open
